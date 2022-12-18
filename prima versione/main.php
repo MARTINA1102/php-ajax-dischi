28826516587bdb6f038b2784665b61e44900d379
MARTINA1102 <martinamiccoli11@gmail.com>
2022-12-18 10:01:55 +0100

    finito

--- prima versione/main.php
+++ prima versione/main.php
@@ -2,12 +2,14 @@
 
 	include_once __DIR__ . '/data/data.php';
 
-	foreach ($arrDischi as $disco) { ?>
-		<div>
-			<img src="<?= $disco['poster'] ?>">
-            <h2><?= $disco['title'] ?></h2>
-			<p><?= $post['author'] ?></p>
-            <p><?= $post['year'] ?></p>
-		</div> <?
-	};?>
+	foreach ($arrDischi as $disco) { 
+		echo "<div class='cards'>";
+			echo "<div class='disco'>";
+				echo "<img class='disco-img src='". $disco["poster"]."'>";
+				echo "<h2 class='titolo'>" . $disco["title"] . "</h2>";
+				echo "<p class='sub_tit'>" . $disco["author"] ."</p>";
+				echo "<p class='sub_tit'>" . $disco["year"]. "</p>";
+			echo "</div>";
+		echo "</div>";
+	} ?>
 </main>
